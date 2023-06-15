940f856ff20bf5467b77cc86a1251e3494a6d5ea
Di-Is <hommageme@gmail.com>
2023-06-15 22:54:04 +0900

    update: config

--- tests/series_curl/entrypoint.sh
+++ tests/series_curl/entrypoint.sh
@@ -2,14 +2,16 @@ mkdir /tests/series_curl/results
 ExeStartTime=`date +%s`
 while true
 do
-	Start10m=`date +%s%N -d "10 minute ago"`
+	Start6m=`date +%s%N -d "6 minute ago"`
+	End3m=`date +%s%N -d "3 minute ago"`
+	Start3m=`date +%s%N -d "3 minute ago"`
 	End0m=`date +%s%N`
-	End5m=`date +%s%N -d "5 minute ago"`
-	RESP0mTo10m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End0m&start=$Start10m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
+
+	Resp0mTo3m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End0m&start=$Start3m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
 	ExeEndTime=`date +%s`
 	RunTime=$((ExeEndTime - ExeStartTime))
-	echo $RunTime $RESP0mTo10m >> /tests/series_curl/results/exe0mto10m.log
-	RESP5mTo10m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End5m&start=$Start10m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
-	echo $RunTime $RESP5mTo10m >> /tests/series_curl/results/exe5mto10m.log
+	echo $RunTime $Resp0mTo3m >> /tests/series_curl/results/exe0mto3m.log
+	Resp3mTo6m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End3m&start=$Start6m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
+	echo $RunTime $Resp3mTo6m >> /tests/series_curl/results/exe3mto6m.log
 	sleep 10
 done
