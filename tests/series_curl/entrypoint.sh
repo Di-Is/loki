fe53ad502a2e4419b5d85bd5c69ad355dc925fa0
DaiIshita <Ishita.Dai@exc.epson.co.jp>
2023-06-16 09:46:32 +0900

    update: test code

--- tests/series_curl/entrypoint.sh
+++ tests/series_curl/entrypoint.sh
@@ -1,17 +1,32 @@
-mkdir /tests/series_curl/results
+RESULT_DIR="/tests/series_curl/results"
+LOG_FILE="series_result.log"
+LOG_PATH="$RESULT_DIR/$LOG_FILE"
+
+if [ ! -d $RESULT_DIR ]; then
+	mkdir $RESULT_DIR
+fi
+
+echo "" > $LOG_PATH
+echo "elapsed_time[m],Now-5mAgo,10mAgo-15mAgo,45mAgo-50mAgo" >> $LOG_PATH
+
+sleep 15
+
 ExeStartTime=`date +%s`
 while true
 do
-	Start6m=`date +%s%N -d "6 minute ago"`
-	End3m=`date +%s%N -d "3 minute ago"`
-	Start3m=`date +%s%N -d "3 minute ago"`
-	End0m=`date +%s%N`
+	T50mAgo=`date +%s%N -d "50 minute ago"`
+	T45mAgo=`date +%s%N -d "45 minute ago"`
+	T15mAgo=`date +%s%N -d "15 minute ago"`
+	T10mAgo=`date +%s%N -d "10 minute ago"`
+	T5mAgo=`date +%s%N -d "5 minute ago"`
+	TNow=`date +%s%N`
 
-	Resp0mTo3m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End0m&start=$Start3m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
+	ResNowTo5mAgo=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$TNow&start=$T5mAgo" --data-urlencode 'match[]={container="test-flog-1",method="GET"}' | jq -r .data[].protocol | jq -csR 'split("\n")[:-1]'`
+	Res10mAgoTo15mAgo=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$T10mAgo&start=$T15mmAgo" --data-urlencode 'match[]={container="test-flog-1",method="GET"}' | jq -r .data[].protocol | jq -csR 'split("\n")[:-1]'`
+	Res45mAgoTo50mAgo=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$T45mAgo&start=$T50mAgo" --data-urlencode 'match[]={container="test-flog-1",method="GET"}' | jq -r .data[].protocol | jq -csR 'split("\n")[:-1]'`
 	ExeEndTime=`date +%s`
 	RunTime=$((ExeEndTime - ExeStartTime))
-	echo $RunTime $Resp0mTo3m >> /tests/series_curl/results/exe0mto3m.log
-	Resp3mTo6m=`curl -s "http://test-loki-1:3100/loki/api/v1/series?end=$End3m&start=$Start6m" --data-urlencode 'match[]={container="test-log-generator-1",status="200"}'`
-	echo $RunTime $Resp3mTo6m >> /tests/series_curl/results/exe3mto6m.log
-	sleep 10
+	RunTime=$((RunTime / 60))
+	echo "$RunTime,$ResNowTo5mAgo,$Res10mAgoTo15mAgo,$Res45mAgoTo50mAgo" >> $LOG_PATH
+	sleep 60
 done
